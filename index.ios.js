78d878241ad72bb8c33bb8fab53cfade68b4b03b
davidhenley <henleydr@gmail.com>
2017-03-19 16:23:39 -0500

    Moving image around

--- index.ios.js
+++ index.ios.js
@@ -10,13 +10,22 @@ import {
 } from 'react-native';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pan: new Animated.ValueXY()
+    }
+  }
+
   componentWillMount() {
     this._panResponder = PanResponder.create({
       onMoveShouldSetResponderCapture: () => true,
       onMoveShouldSetPanResponderCapture: () => true,
       onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setValue({ x: 0, y: 0 });
       },
       onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y}
       ]),
       onPanResponderRelease: (e, {vx, vy}) => {
       }
@@ -24,9 +33,12 @@ class App extends Component {
   }
 
   render() {
+    let { pan } = this.state;
+    let [ translateX, translateY ] = [pan.x, pan.y];
+    let imageStyle = { transform: [{translateX}, {translateY}]};
     return (
       <View style={styles.container}>
-        <Animated.View {...this._panResponder.panHandlers}>
+        <Animated.View style={imageStyle} {...this._panResponder.panHandlers}>
           <Image source={require('./assets/target.png')} />
         </Animated.View>
       </View>
